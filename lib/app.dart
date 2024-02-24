42977675927dc88a455526bc2e9e674e366c7c8e
tiaditaa <118504168+tiaditaa@users.noreply.github.com>
2024-02-24 23:19:07 +0700

    codelabs 4

--- lib/app.dart
+++ lib/app.dart
@@ -13,15 +13,30 @@
 // limitations under the License.
 
 import 'package:flutter/material.dart';
-import 'supplemental/cut_corners_border.dart';
-
 import 'colors.dart';
+import 'category_menu_page.dart';
 import 'home.dart';
 import 'login.dart';
+import 'supplemental/cut_corners_border.dart';
+import 'backdrop.dart'; // New code
+import 'model/product.dart'; // New code
 
 // TODO: Convert ShrineApp to stateful widget (104)
-class ShrineApp extends StatelessWidget {
+class ShrineApp extends StatefulWidget {
   const ShrineApp({Key? key}) : super(key: key);
+
+  @override
+  State<ShrineApp> createState() => _ShrineAppState();
+}
+
+class _ShrineAppState extends State<ShrineApp> {
+  Category _currentCategory = Category.all;
+
+  void _onCategoryTap(Category category) {
+    setState(() {
+      _currentCategory = category;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,12 +46,25 @@ class ShrineApp extends StatelessWidget {
       routes: {
         '/login': (BuildContext context) => const LoginPage(),
         // TODO: Change to a Backdrop with a HomePage frontLayer (104)
-        '/': (BuildContext context) => const HomePage(),
+        '/': (BuildContext context) => Backdrop(
+              // TODO: Make currentCategory field take _currentCategory (104)
+              currentCategory: Category.all,
+              // TODO: Pass _currentCategory for frontLayer (104)
+              frontLayer: HomePage(category: _currentCategory),
+              // TODO: Change backLayer field value to CategoryMenuPage (104)
+              backLayer: CategoryMenuPage(
+                currentCategory: _currentCategory,
+                onCategoryTap: _onCategoryTap,
+              ),
+              frontTitle: Text('SHRINE'),
+              backTitle: Text('MENU'),
+            ),
         // TODO: Make currentCategory field take _currentCategory (104)
         // TODO: Pass _currentCategory for frontLayer (104)
         // TODO: Change backLayer field value to CategoryMenuPage (104)
       },
-      theme: _kShrineTheme,
+      // TODO: Customize the theme (103)
+      theme: _kShrineTheme, // New code
     );
   }
 }
@@ -56,7 +84,10 @@ ThemeData _buildShrineTheme() {
     textSelectionTheme: const TextSelectionThemeData(
       selectionColor: kShrinePink100,
     ),
-    // TODO: Decorate the inputs (103)
+    appBarTheme: const AppBarTheme(
+      foregroundColor: kShrineBrown900,
+      backgroundColor: kShrinePink100,
+    ),
     inputDecorationTheme: const InputDecorationTheme(
       border: CutCornersBorder(),
       focusedBorder: CutCornersBorder(
@@ -96,3 +127,6 @@ TextTheme _buildShrineTextTheme(TextTheme base) {
         bodyColor: kShrineBrown900,
       );
 }
+
+// TODO: Build a Shrine Theme (103)
+// TODO: Build a Shrine Text Theme (103)
